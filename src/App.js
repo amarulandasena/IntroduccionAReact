f7624324c9edb381eefaa24d07f35fec58c3633b
Andres_Marulanda <afmsamsung1981@gmail.com>
2024-07-25 09:09:11 -0500

    Se termina de implementar el proyecto, solo falta asignar la proiedad que lo haga responsive

--- src/App.js
+++ src/App.js
@@ -4,7 +4,33 @@ import Testimonio from './componentes/Testimonio.js';
 function App() {
   return (
     <div className="App">
-        <Testimonio />
+      <div className='contenedorPrincipal'>
+        <h1>
+            Esto es lo que dicen nuestros alumnos sobre Freecodecamp.
+        </h1>
+        <Testimonio 
+          nombre = 'Shawn Wang'
+          pais = 'Singapur'
+          imagen = 'testimonio1'
+          cargo = 'Ingeniero de software'
+          empresa = 'Amazon'
+          testimonio = 'Cambiar de profesión da miedo. Solo gané confianza en que podía codificar trabajando a través de los cientos de horas gratuitas de freecodecamp. En un años tenía un trabajo de 6 cifras como Ingeniero de Software. Freecodecamp cambió mi vida.' />
+        <Testimonio 
+          nombre = 'Sarah Chima'
+          pais = 'Nigeria'
+          imagen = 'testimonio2'
+          cargo = 'Ingeniera de software'
+          empresa = 'ChatDesk'
+          testimonio = 'freeCodeCamp fue la puerta de entrada a mi carrera como desarrollador de software. El plan de estudios bien estructurado llevó mis conocimientos de programación de un nivel de principiante total a un nivel muy seguro. Era todo lo que necesitaba para conseguir mi primer trabajo de desarrollador en una empresa increíble.'/>
+        <Testimonio 
+          nombre = 'Emma Bostian'
+          pais = 'Suecia'
+          imagen = 'testimonio3'
+          cargo = 'Ingeniera de software'
+          empresa = 'Spotify'
+          testimonio = 'Siempre he tenido problemas para aprender JavaScript. He tomado muchos cursos, pero el curso de freeCodeCamp fue el que se quedó. Estudiar JavaScript, así como estructuras de datos y algoritmos en freeCodeCamp me dio las habilidades y la confianza que necesitaba para conseguir el trabajo de mis sueños como ingeniero de software en Spotify.'/>
+      </div>
+        
     </div>
   );
 }
--- src/componentes/Testimonio.js
+++ src/componentes/Testimonio.js
@@ -1,19 +1,22 @@
 import React from 'react';
+import '../css/Testimonio.css';
 
-function Testimonio () {
+function Testimonio (props) {
     return (
         <div className = 'contenedorTestimonio'>
             <aside>
                 <img className = 'imagenTestimonio'
-                     src = {require('../imagenes/testimonio1.jpeg')}
+                     src = {require(`../imagenes/${props.imagen}.jpeg`)}
                      alt = 'Foto George'/>
             </aside>
             <section className='contenedorTextoTestimonio'>
-                <p className='nombreTestimonio'>Shawn Wang en Singapur</p>
-                <p className='cargoTestimonio'>Ingeniero de Software en Amazon</p>
-                <p className='textoTestimonio'>Cambiar de profesión da miedo. Sólo gané confianza en que podía codificar trabajando a 
-                    través de los cientos de horas de lecciones gratuitas en freeCodeCamp. En un año tenía un trabajo de seis cifras 
-                    como Ingeniero de Software. freeCodeCamp cambió mi vida.</p>
+                <p className='nombreTestimonio'>
+                    <strong>{props.nombre}</strong> en {props.pais}
+                </p>
+                <p className='cargoTestimonio'>
+                    {props.cargo} en <strong>{props.empresa}</strong>
+                </p>
+                <p className='textoTestimonio'>"{props.testimonio}"</p>
             </section>
         </div>
     );
